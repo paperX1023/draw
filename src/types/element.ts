2edc3a9b438250603c98ce703ef2c075f4ebc3cc
TianMing <3042568013@qq.com>
2025-11-29 13:53:50 +0800

    定义element接口

---
+++ src/types/element.ts
@@ -0,0 +1,34 @@
+export type ElementType = "rect" | "circle" | "triangle" | "image" | "text";
+
+export interface IElementStyle {
+  fillColor: string | null;
+  lineColor: string;
+  lineWidth: number;
+  fontSize?: number;
+  fontFamily?: string;
+  fontColor?: string;
+}
+
+export interface IFilters {
+  blur: number;
+  brightness: number;
+  contrast: number;
+}
+
+export interface IElement {
+  readonly id: string;
+  type: ElementType;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  rotation: number;
+  visible: boolean;
+  locked: boolean;
+
+  text?: string;
+  src?: string;
+  filters?: IFilters;
+
+  style: IElementStyle;
+}
